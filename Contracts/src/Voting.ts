b0a9201f97592400cbcdffdce4003d76e2027225
kadirchan <kadircanbozkurt1905@gmail.com>
2024-07-23 02:30:28 +0300

    contract skeleton

--- Contracts/src/Voting.ts
+++ Contracts/src/Voting.ts
@@ -1,9 +1,48 @@
-import { Field, SmartContract, State, state } from 'o1js';
+import { Field, method, SmartContract, Permissions, State, state } from 'o1js';
 
 export class Voting extends SmartContract {
   @state(Field) nullifierRoot = State<Field>();
 
   @state(Field) votersRoot = State<Field>();
 
+  /**
+   * 0: Not started
+   * 1: Started
+   * 2: Finalized
+   */
   @state(Field) status = State<Field>();
+
+  @state(Field) votingID = State<Field>();
+
+  async deploy() {
+    await super.deploy();
+    this.account.permissions.set({
+      ...Permissions.default(),
+      editState: Permissions.proofOrSignature(),
+      editActionState: Permissions.proofOrSignature(),
+      incrementNonce: Permissions.proofOrSignature(),
+      setPermissions: Permissions.proofOrSignature(),
+    });
+  }
+
+  @method
+  async setVotingID(id: Field) {
+    this.status.getAndRequireEquals().assertEquals(Field.from(0));
+    this.votingID.set(id);
+  }
+
+  @method
+  async voterRegistration(votersRoot: Field) {
+    this.status.getAndRequireEquals().assertEquals(Field.from(0));
+    this.votersRoot.set(votersRoot);
+  }
+
+  @method
+  async submitAggregatedVotes() {}
+
+  @method
+  async startVoting() {}
+
+  @method
+  async finalizeVoting() {}
 }
