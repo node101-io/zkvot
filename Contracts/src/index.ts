98f43247ae9c32a2d24f817c8ba2d95a282e875e
kadirchan <kadircanbozkurt1905@gmail.com>
2024-08-02 19:35:46 +0300

    benchmark

--- Contracts/src/index.ts
+++ Contracts/src/index.ts
@@ -1,5 +1,4 @@
 import {
-  Nullifier,
   Field,
   MerkleMap,
   Mina,
@@ -7,11 +6,12 @@ import {
   PrivateKey,
   PublicKey,
   Poseidon,
-  Provable,
   verify,
 } from 'o1js';
-import { Vote, VotePrivateInputs, VotePublicInputs } from './NewVote.js';
+
 import { MerkleWitnessClass } from './utils.js';
+
+import { Vote, VotePrivateInputs, VotePublicInputs } from './NewVote.js';
 import {
   VoteAggregator,
   VoteAggregatorPublicInputs,
@@ -26,8 +26,8 @@ for (let i = 0; i < 10; i++) {
   let privateKey = PrivateKey.random();
   let publicKey = privateKey.toPublicKey();
   votersArray.push([privateKey, publicKey]);
-  console.log(`Voter ${i} private key: ${privateKey.toBase58()}`);
-  console.log(`Voter ${i} public key: ${publicKey.toBase58()}`);
+  // console.log(`Voter ${i} private key: ${privateKey.toBase58()}`);
+  // console.log(`Voter ${i} public key: ${publicKey.toBase58()}`);
 }
 
 votersArray.sort((a) => {
@@ -40,10 +40,10 @@ votersArray.sort((a) => {
   return 0;
 });
 
-console.log('Sorted public keys:');
+// console.log('Sorted public keys:');
 
 for (let i = 0; i < 10; i++) {
-  console.log(`Voter ${i} public key: ${votersArray[i][1].toBase58()}`);
+  // console.log(`Voter ${i} public key: ${votersArray[i][1].toBase58()}`);
 }
 
 let votersTree = new MerkleTree(32);
@@ -63,9 +63,9 @@ console.log('verification key', verificationKey.data.slice(0, 10) + '..');
 console.log('casting votes');
 let voteProofs = [];
 
-let votingId = Field.random();
+let votingId = Field.from(123);
 
-for (let i = 0; i < 9; i++) {
+for (let i = 0; i < 3; i++) {
   let vote = BigInt((i % 2) + 1);
   let privateKey = votersArray[i][0];
   let merkleTreeWitness = votersTree.getWitness(BigInt(i));
@@ -73,23 +73,25 @@ for (let i = 0; i < 9; i++) {
 
   let votePublicInputs = new VotePublicInputs({
     votingId: votingId,
-    vote: Field.from(vote),
     votersRoot: votersRoot,
   });
   let votePrivateInputs = new VotePrivateInputs({
     privateKey: privateKey,
+    vote: Field.from(vote),
     votersMerkleWitness: witness,
   });
 
+  let time = Date.now();
   let voteProof = await Vote.vote(votePublicInputs, votePrivateInputs);
   // console.log(`Vote ${i} proof: ${JSON.stringify(voteProof.toJSON())}`);
-  console.log(
-    `vote casted by voter ${i} with vote ${voteProof.publicOutput.vote.toString()}`
-  );
+  // console.log(
+  //   `vote casted by voter ${i} with vote ${voteProof.publicOutput.vote.toString()}`
+  // );
+  console.log(`vote ${i} proof took ${(Date.now() - time) / 1000} seconds `);
   voteProofs.push(voteProof);
 }
 
-console.log('vote aggregator digest', await VoteAggregator.digest());
+// console.log('vote aggregator digest', await VoteAggregator.digest());
 
 console.log('compiling vote aggregator program');
 
@@ -114,7 +116,7 @@ console.log('aggregating votes');
 
 let previousProof = baseProof;
 
-for (let i = 0; i < 9; i++) {
+for (let i = 0; i < 3; i++) {
   let voteProof = voteProofs[i];
   let voteId = votingId;
   countedVotersMapRoot = countedVotersMap.getRoot();
@@ -128,22 +130,24 @@ for (let i = 0; i < 9; i++) {
     merkleWitness: countedVotersMapWitness,
   });
 
+  let time = Date.now();
   let proof = await VoteAggregator.aggregateVotes(
     publicInput,
     previousProof,
     voteProof
   );
-  console.log(
-    `After vote ${i} yays: ${proof.publicOutput.yeys.toString()}, nays: ${proof.publicOutput.nays.toString()}`
-  );
+  console.log(`Vote ${i} proof took ${(Date.now() - time) / 1000} seconds `);
+  // console.log(
+  //   `After vote ${i} yays: ${proof.publicOutput.yeys.toString()}, nays: ${proof.publicOutput.nays.toString()}`
+  // );
   let ok = await verify(proof, voteAggregatorVerificationKey);
   console.log(`Vote ${i} proof verified: ${ok}`);
   if (!ok) {
     throw new Error(`Vote ${i} proof failed to verify`);
   }
-  console.log(
-    `Vote ${i} total aggregated count: ${proof.publicOutput.totalAggregatedCount.toString()}`
-  );
+  // console.log(
+  //   `Vote ${i} total aggregated count: ${proof.publicOutput.totalAggregatedCount.toString()}`
+  // );
   previousProof = proof;
   countedVotersMap.set(
     voteProof.publicOutput.identifierHash,
