48fedf868c0e7bd87315ef5312bfd9640f16b548
Ahmed Zeyad <keyboord01@gmail.com>
2024-10-20 17:41:07 +0300

    CID Transaction id controlling fix and raw image rename

--- app/app/(pages)/createAnElection/steps/StepSix.jsx
+++ app/app/(pages)/createAnElection/steps/StepSix.jsx
@@ -26,37 +26,20 @@ const StepSix = ({ electionData, onPrevious, onSubmit, onDownload }) => {
     setErrorMessage("");
   };
 
-  console.log("electionData.storageLayer:", electionData.storageLayer);
-
-  const isValidTransactionId = (transactionId) => {
-    switch (electionData.storageLayer) {
-      case "arweave":
-        return /^[a-zA-Z0-9_-]{43,}$/.test(transactionId);
-      case "ipfs":
-        return /^[a-zA-Z0-9]{46}$/.test(transactionId);
-      case "filecoin":
-        return /^[a-zA-Z0-9]{59}$/.test(transactionId);
-      default:
-        return false;
-    }
-  };
-
   const handleSubmit = () => {
     if (isSubmitEnabled) {
-      if (!isValidTransactionId(transactionId.trim())) {
-        setErrorMessage("Please enter a valid transaction ID.");
-        return;
-      }
-
-      setErrorMessage("");
       setIsFetchingData(true);
 
-      onSubmit(transactionId.trim())
+      Promise.resolve(onSubmit(transactionId.trim(), setErrorMessage))
         .then(() => {
-          onSubmit();
+          console.log("Submission successful.");
         })
         .catch((error) => {
           console.error("Submission error:", error);
+          const errorMessageToDisplay =
+            error.message || "An error occurred during submission.";
+          setErrorMessage(errorMessageToDisplay);
+          console.log("Displaying error message:", errorMessageToDisplay); // Log the error message directly
         })
         .finally(() => {
           setIsFetchingData(false);
@@ -176,7 +159,6 @@ function getStepsData(storageLayer, onDownload) {
           ),
           image: DownloadFileImage,
         },
-
         {
           text: "6. Click on the file you uploaded to see its content.",
           image: FileContentImage,
@@ -184,9 +166,9 @@ function getStepsData(storageLayer, onDownload) {
         {
           text: (
             <>
-              7. Click on “info” button on right to see the file info. Copy the
-              URL starting with “https://arweave.net” and paste it into the
-              below field.
+              7. Click on “info” button on the right to see the file info. Copy
+              the URL starting with “https://arweave.net” and paste it into the
+              field below.
             </>
           ),
           image: CopyFromFileImage,
