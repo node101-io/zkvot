a1eebaee67f8c8caf05d5108c823eaa132c8d271
Necip Baha Sağıroğlu <sagiroglun22@itu.edu.tr>
2024-10-20 16:18:38 +0300

    draft

--- app/electron/controllers/generateVoteProofController.js
+++ app/electron/controllers/generateVoteProofController.js
@@ -1,8 +1,7 @@
-// import { Request, Response } from 'express';
 // import { Vote, VoteProof } from 'contracts';
 
 // const { verificationKey } = await Vote.compile();
-
+// console.log(verificationKey);
 export default (req, res) => {
   // console.log(verificationKey);
   // TODO: Implement generateVoteProofController
