b1f3bf69475c16fc93039ee30812b8d251692360
Yunus Gürlek <yunusguerlek@gmail.com>
2024-10-21 19:40:08 +0200

    Small update in verification of votes

--- backend/src/models/vote/functions/verifyVote.ts
+++ backend/src/models/vote/functions/verifyVote.ts
@@ -1,22 +1,32 @@
-import { JsonProof } from 'o1js';
-import { VoteProof } from 'contracts';
+import { JsonProof, verify } from 'o1js';
+import { VoteProof, Vote } from 'contracts';
+
+const { verificationKey } = await Vote.compile();
 
 export default (
   data: {
     vote: JsonProof;
+    electionId: string;
     merkleRoot: bigint;
   },
   callback: (error: string | null, nullifier?: undefined | bigint) => any
 ) => {
+  if (!verificationKey)
+    return callback('proof_not_compiled');
+
   VoteProof
     .fromJSON(data.vote)
-    .then(voteProof => {
-      voteProof.verify();
-
+    .then(async (voteProof) => {
       if (voteProof.publicInput.votersRoot.toBigInt() != data.merkleRoot)
+        return callback('invalid_proof');
+
+      if (voteProof.publicInput.electionId.toBase58() != data.electionId)
+        return callback('invalid_proof');
+
+      if (!(await verify(voteProof, verificationKey)))
         return callback('invalid_proof');
 
       return callback(null, voteProof.publicOutput.nullifier.toBigInt());
     })
-    .catch(err => callback('invalid_proof'));
+    .catch(_ => callback('invalid_proof'));
 };
