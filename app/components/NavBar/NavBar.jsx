34dd2a11c10cb7e6044417c64674f8b95732ce6a
Ahmed Zeyad <keyboord01@gmail.com>
2024-10-22 22:35:03 +0300

    wallet selection modal fix

--- app/components/NavBar/NavBar.jsx
+++ app/components/NavBar/NavBar.jsx
@@ -2,7 +2,6 @@
 import React, { useEffect, useState } from "react";
 import WalletButton from "../common/WalletButton";
 import { motion } from "framer-motion";
-import Logo from "../../assets/Logo.svg";
 import Image from "next/image";
 import ZKVot from "@/assets/Logo/ZKVot.svg";
 import ZKVotDot from "@/assets/Logo/ZKVotDot.svg";
