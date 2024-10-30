1a691e1ff0c732be06266deabb4d89b4f7eb62b5
Necip Baha Sağıroğlu <sagiroglun22@itu.edu.tr>
2024-10-30 19:20:37 +0300

    turbo draft

--- contracts/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { ElectionContract, setElectionContractConstants } from './ElectionContract.js';
-import { Vote, VoteProof, VotePrivateInputs, VotePublicInputs } from './VoteProgram.js';
-import { MerkleWitnessClass } from './utils.js';
-import { AggregateProof, RangeAggregationProgram } from './RangeAggregationProgram.js';
-
-export { AggregateProof, Vote, VoteProof, VotePrivateInputs, VotePublicInputs, MerkleWitnessClass, RangeAggregationProgram, ElectionContract, setElectionContractConstants };
