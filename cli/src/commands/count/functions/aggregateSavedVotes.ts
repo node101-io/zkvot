d5cc47c78d6eda4306dcfedbf8b6bfed539ce7f5
Necip Baha Sağıroğlu <sagiroglun22@itu.edu.tr>
2024-10-30 16:31:49 +0300

    feat: Refactor contract exports & Adjust aggregation for saved vote proofs (#43)
    Co-authored-by: Yakup Altay <yakup.altay@boun.edu.tr>

--- cli/src/commands/count/functions/aggregateSavedVotes.ts
+++ cli/src/commands/count/functions/aggregateSavedVotes.ts
@@ -1,199 +1,348 @@
-import { AggregateProof, RangeAggregationProgram, VoteProof } from "contracts";
-import { InnerNode, LeafNode, SegmentTree } from "contracts/src/SegmentTree";
-import { Level } from "level";
-import { Field, JsonProof, PublicKey } from "o1js";
+import { AggregateProof, InnerNode, LeafNode, RangeAggregationProgram, SegmentTree, Vote, VoteProof } from 'contracts';
+import { Field, JsonProof, PublicKey } from 'o1js';
+import { EntryStream } from 'level-read-stream';
+import { Level } from 'level';
+import async from 'async';
 
-const db = new Level("./cachedProofsDb", { valueEncoding: "json" });
+const db = new Level('./cachedProofsDb', { valueEncoding: 'json' });
 
-/**
- * Compile that programs before call this function
- * - Vote
- * - RangeAggregationProgram
- *
- * @param electionId public key of the election
- * @param voteProofs list of vote proofs (make sure to verify them before calling this function)
- * @param votersRoot root of the voters merkle tree
- * @param callback
- *
- * @returns aggregate proof of the election as JSON
- */
-export default async (
-    electionId: PublicKey,
-    voteProofs: JsonProof[],
-    votersRoot: Field,
-    callback: any
+const buildTheSegmentTreeOfVoteProofs = (
+  voteProofs: JsonProof[],
+  callback: (
+    err: Error | string | null,
+    segmentTree?: SegmentTree<bigint, unknown, VoteProof>,
+  ) => void
 ) => {
-    const leaves: LeafNode<bigint, VoteProof>[] = [];
+  const segmentTreeLeaves: LeafNode<bigint, VoteProof>[] = [];
 
-    for (let i = 0; i < voteProofs.length; i++) {
-        const voteProof = await VoteProof.fromJSON(voteProofs[i]);
-        const nullifier = voteProof.publicOutput.nullifier.toBigInt();
+  async.each(
+    voteProofs,
+    (
+      voteProofJson: JsonProof,
+      next: (err: Error | string | null) => void
+    ) => {
+      VoteProof.fromJSON(voteProofJson)
+        .then((voteProof: VoteProof) => {
+          const nullifier = voteProof.publicOutput.nullifier.toBigInt();
+          const segmentTreeLeaf = new LeafNode(nullifier, voteProof);
 
-        const leaf = new LeafNode(nullifier, voteProof);
-        leaves.push(leaf);
+          segmentTreeLeaves.push(segmentTreeLeaf);
+          next(null);
+        })
+        .catch(err => next(err));
+    },
+    err => {
+      if (err)
+        return callback(err);
+
+      return callback(null, SegmentTree.build(segmentTreeLeaves));
     }
+  );
+};
 
-    const segmentTree = SegmentTree.build(leaves);
+const loadCachedAggregatorProofs = (
+  segmentTree: SegmentTree<bigint, unknown, VoteProof>,
+  callback: (err: Error | string | null) => void
+) => {
+  const cachedAggregatorProofs: { includedVotesHash: bigint; proof: string }[] = [];
 
-    try {
-        console.log("Loading cached aggregator proofs");
-        const mappings = [];
+  (new EntryStream(db))
+    .on('data', entry => {
+      cachedAggregatorProofs.push({
+        includedVotesHash: BigInt(entry.key),
+        proof: entry.value,
+      });
+    })
+    .on('error', err => callback(err))
+    .on('end', () => {
+      if (cachedAggregatorProofs.length === 0)
+        return callback(null);
 
-        for await (const [key, value] of db.iterator()) {
-            mappings.push({ includedVotesHash: key, proof: value });
+      async.each(
+        cachedAggregatorProofs,
+        (
+          cachedAggregatorProof: { includedVotesHash: bigint, proof: string },
+          next: (err: Error | string | null) => void
+        ) => {
+          const includedVotesHash = cachedAggregatorProof.includedVotesHash;
+          const voteProofJson = JSON.parse(cachedAggregatorProof.proof);
+
+          AggregateProof.fromJSON(voteProofJson)
+            .then((aggregatedProof: AggregateProof) => {
+              segmentTree.cachedAggregatorProofs.set(includedVotesHash, aggregatedProof);
+
+              next(null);
+            })
+            .catch(err => next(err));
+        },
+        err => {
+          if (err)
+            return callback(err);
+
+          return callback(null);
         }
+      );
+    });
+};
 
-        if (mappings.length === 0) {
-            console.log("No cached aggregator proofs found");
-        } else {
-            console.log("Cached aggregator proofs found:", mappings.length);
-        }
+const aggregateNodeProofs = (
+  data: {
+    node: any,
+    votersRoot: Field,
+    electionId: PublicKey,
+    segmentTree: SegmentTree<bigint, unknown, VoteProof>
+  },
+  callback: (
+    err: Error | string | null,
+    aggregateProof?: AggregateProof
+  ) => void
+) => {
+  const leftChild = data.node.leftChild;
+  const rightChild = data.node.rightChild;
+  const includedVotesHash = SegmentTree.includedVotesHash(data.node.includedVotes);
 
-        for (let i = 0; i < mappings.length; i++) {
-            const mapping = mappings[i];
-            const includedVotesHash = BigInt(mapping.includedVotesHash);
-            const proofJson = JSON.parse(mapping.proof);
-            const proof = await AggregateProof.fromJSON(proofJson);
-            segmentTree.cachedAggregatorProofs.set(includedVotesHash, proof);
-        }
-    } catch (e) {
-        console.log("Error loading cached aggregator proofs", e);
+  if (data.segmentTree.cachedAggregatorProofs.has(includedVotesHash))
+    return callback('duplicated_aggregate_proof');
+
+  let aggregateProof: AggregateProof | undefined;
+
+  if (leftChild && rightChild) {
+    if (leftChild instanceof LeafNode && rightChild instanceof LeafNode) {
+      return RangeAggregationProgram.base_two(
+        {
+          votersRoot: data.votersRoot,
+          electionId: data.electionId,
+        },
+        leftChild.voteProof,
+        rightChild.voteProof
+      )
+        .then((proof: AggregateProof) => callback(null, proof))
+        .catch(err => callback(err));
+    } else if (leftChild instanceof LeafNode && rightChild instanceof InnerNode && leftChild.voteProof instanceof VoteProof) {
+      const rightChildAggregatorProof = data.segmentTree.getCachedAggregatorProof(
+        SegmentTree.includedVotesHash(rightChild.includedVotes) as bigint
+      );
+
+      return RangeAggregationProgram.append_left(
+        {
+          votersRoot: data.votersRoot,
+          electionId: data.electionId
+        },
+        rightChildAggregatorProof as AggregateProof,
+        leftChild.voteProof
+      )
+        .then((proof: AggregateProof) => callback(null, proof))
+        .catch(err => callback(err));
+    } else if (leftChild instanceof InnerNode && rightChild instanceof LeafNode && rightChild.voteProof instanceof VoteProof) {
+      const leftChildAggregatorProof = data.segmentTree.getCachedAggregatorProof(
+        SegmentTree.includedVotesHash(leftChild.includedVotes) as bigint
+      );
+
+      return RangeAggregationProgram.append_right(
+        {
+          votersRoot: data.votersRoot,
+          electionId: data.electionId
+        },
+        leftChildAggregatorProof as AggregateProof,
+        rightChild.voteProof
+      )
+        .then((proof: AggregateProof) => callback(null, proof))
+        .catch(err => callback(err));
+    } else if (leftChild instanceof InnerNode && rightChild instanceof InnerNode) {
+      const leftChildAggregatorProof = data.segmentTree.getCachedAggregatorProof(
+        SegmentTree.includedVotesHash(leftChild.includedVotes) as bigint
+      );
+
+      const rightChildAggregatorProof = data.segmentTree.getCachedAggregatorProof(
+        SegmentTree.includedVotesHash(rightChild.includedVotes) as bigint
+      );
+
+      return RangeAggregationProgram.merge(
+        {
+          votersRoot: data.votersRoot,
+          electionId: data.electionId
+        },
+        leftChildAggregatorProof as AggregateProof,
+        rightChildAggregatorProof as AggregateProof
+      )
+        .then((proof: AggregateProof) => callback(null, proof))
+        .catch(err => callback(err));
     }
-
-    const aggregateOrder = segmentTree.traverse();
-
-    for (let i = 0; i < aggregateOrder.length; i++) {
-        console.time(`Aggregating node ${i} of ${aggregateOrder.length}`);
-        const node = aggregateOrder[i];
-        const leftChild = node.leftChild;
-        const rightChild = node.rightChild;
-        const includedVotesHash = SegmentTree.includedVotesHash(node.includedVotes);
-
-        let aggregateProof;
-
-        if (segmentTree.cachedAggregatorProofs.has(includedVotesHash)) {
-            console.log("Cache hit for node", i);
-            continue;
-        }
-        if (leftChild && rightChild) {
-            if (leftChild instanceof LeafNode && rightChild instanceof LeafNode) {
-                aggregateProof = await RangeAggregationProgram.base_two(
-                    {
-                        votersRoot: votersRoot,
-                        electionId,
-                    },
-                    leftChild.voteProof,
-                    rightChild.voteProof
-                );
-            } else if (
-                leftChild instanceof LeafNode &&
-                rightChild instanceof InnerNode &&
-                leftChild.voteProof instanceof VoteProof
-            ) {
-                const rightChildAggregatorProof = segmentTree.getCachedAggregatorProof(
-                    SegmentTree.includedVotesHash(rightChild.includedVotes) as bigint
-                );
-
-                aggregateProof = await RangeAggregationProgram.append_left(
-                    {
-                        votersRoot: votersRoot,
-                        electionId,
-                    },
-                    rightChildAggregatorProof as AggregateProof,
-                    leftChild.voteProof
-                );
-            } else if (
-                leftChild instanceof InnerNode &&
-                rightChild instanceof LeafNode &&
-                rightChild.voteProof instanceof VoteProof
-            ) {
-                const leftChildAggregatorProof = segmentTree.getCachedAggregatorProof(
-                    SegmentTree.includedVotesHash(leftChild.includedVotes) as bigint
-                );
-
-                aggregateProof = await RangeAggregationProgram.append_right(
-                    {
-                        votersRoot: votersRoot,
-                        electionId,
-                    },
-                    leftChildAggregatorProof as AggregateProof,
-                    rightChild.voteProof
-                );
-            } else if (leftChild instanceof InnerNode && rightChild instanceof InnerNode) {
-                const leftChildAggregatorProof = segmentTree.getCachedAggregatorProof(
-                    SegmentTree.includedVotesHash(leftChild.includedVotes) as bigint
-                );
-                const rightChildAggregatorProof = segmentTree.getCachedAggregatorProof(
-                    SegmentTree.includedVotesHash(rightChild.includedVotes) as bigint
-                );
-
-                aggregateProof = await RangeAggregationProgram.merge(
-                    {
-                        votersRoot: votersRoot,
-                        electionId,
-                    },
-                    leftChildAggregatorProof as AggregateProof,
-                    rightChildAggregatorProof as AggregateProof
-                );
-            }
-        } else if (leftChild) {
-            if (leftChild instanceof InnerNode) {
-                aggregateProof = segmentTree.getCachedAggregatorProof(
-                    SegmentTree.includedVotesHash(leftChild.includedVotes) as bigint
-                );
-            } else if (leftChild instanceof LeafNode && leftChild.voteProof instanceof VoteProof) {
-                aggregateProof = await RangeAggregationProgram.base_one(
-                    {
-                        votersRoot: votersRoot,
-                        electionId,
-                    },
-                    leftChild.voteProof
-                );
-            }
-        } else if (rightChild) {
-            if (rightChild instanceof InnerNode) {
-                aggregateProof = segmentTree.getCachedAggregatorProof(
-                    SegmentTree.includedVotesHash(rightChild.includedVotes) as bigint
-                );
-            } else if (
-                rightChild instanceof LeafNode &&
-                rightChild.voteProof instanceof VoteProof
-            ) {
-                aggregateProof = await RangeAggregationProgram.base_one(
-                    {
-                        votersRoot: votersRoot,
-                        electionId,
-                    },
-                    rightChild.voteProof
-                );
-            }
-        }
-
-        segmentTree.cachedAggregatorProofs.set(
-            SegmentTree.includedVotesHash(node.includedVotes),
-            aggregateProof
-        );
-
-        const includedVotesHashString = includedVotesHash.toString();
-        const proofJson = (aggregateProof as AggregateProof).toJSON();
-        const proofString = JSON.stringify(proofJson);
-
-        try {
-            await db.put(includedVotesHashString, proofString);
-            console.log("Cached proof saved to LevelDB");
-        } catch (e) {
-            console.log("Error saving cached proof to LevelDB", e);
-        }
-        console.timeEnd(`Aggregating node ${i} of ${aggregateOrder.length}`);
+  } else if (leftChild) {
+    if (leftChild instanceof InnerNode) {
+      aggregateProof = data.segmentTree.getCachedAggregatorProof(
+        SegmentTree.includedVotesHash(leftChild.includedVotes) as bigint
+      ) as AggregateProof;
+    } else if (leftChild instanceof LeafNode && leftChild.voteProof instanceof VoteProof) {
+      return RangeAggregationProgram.base_one(
+        {
+          votersRoot: data.votersRoot,
+          electionId: data.electionId
+        },
+        leftChild.voteProof
+      )
+        .then((proof: AggregateProof) => callback(null, proof))
+        .catch(err => callback(err));
     }
-
-    if (segmentTree.root instanceof InnerNode) {
-        const rootAggregatorProof = segmentTree.getCachedAggregatorProof(
-            SegmentTree.includedVotesHash(segmentTree.root.includedVotes) as bigint
-        ) as AggregateProof;
-
-        return callback(rootAggregatorProof.toJSON());
+  } else if (rightChild) {
+    if (rightChild instanceof InnerNode) {
+      aggregateProof = data.segmentTree.getCachedAggregatorProof(
+        SegmentTree.includedVotesHash(rightChild.includedVotes) as bigint
+      ) as AggregateProof;
+    } else if (rightChild instanceof LeafNode && rightChild.voteProof instanceof VoteProof) {
+      return RangeAggregationProgram.base_one(
+        {
+          votersRoot: data.votersRoot,
+          electionId: data.electionId
+        },
+        rightChild.voteProof
+      )
+        .then((proof: AggregateProof) => callback(null, proof))
+        .catch(err => callback(err));
     }
+  };
+
+  return callback('aggregate_error');
+};
+
+const saveAggregateVoteProofToCache = (
+  data: {
+    includedVotesHash: bigint,
+    aggregateProof: AggregateProof,
+    segmentTree: SegmentTree<bigint, unknown, VoteProof>
+  },
+  callback: (err: Error | string | null) => void
+) => {
+  data.segmentTree.cachedAggregatorProofs.set(
+    data.includedVotesHash,
+    data.aggregateProof
+  );
+
+  const includedVotesHashString = data.includedVotesHash.toString();
+  const aggregateProofJson = data.aggregateProof.toJSON();
+  const aggregateProofString = JSON.stringify(aggregateProofJson);
+
+  db.put(includedVotesHashString, aggregateProofString, err => {
+    if (err)
+      return callback(err);
 
     return callback(null);
+  });
+};
+
+const processSegmentTreeForAggregation = (
+  data: {
+    segmentTree: SegmentTree<bigint, unknown, VoteProof>,
+    votersRoot: Field,
+    electionId: PublicKey
+  },
+  callback: (err: Error | string | null) => void
+) => {
+  const aggregateOrder: InnerNode<bigint, VoteProof>[] = data.segmentTree.traverse();
+
+  async.eachSeries(
+    aggregateOrder,
+    (
+      node: InnerNode<bigint, VoteProof>,
+      next: (err: Error | string | null) => void
+    ) => {
+      aggregateNodeProofs(
+        {
+          node,
+          votersRoot: data.votersRoot,
+          electionId: data.electionId,
+          segmentTree: data.segmentTree
+        },
+        (err, aggregateProof) => {
+          if (err)
+            return next(err);
+
+          if (!aggregateProof)
+            return next('aggregate_error');
+
+          saveAggregateVoteProofToCache(
+            {
+              includedVotesHash: SegmentTree.includedVotesHash(node.includedVotes),
+              aggregateProof,
+              segmentTree: data.segmentTree
+            },
+            err => {
+              if (err)
+                return next(err);
+
+              return next(null);
+            }
+          );
+        }
+      );
+    },
+    err => {
+      if (err)
+        return callback(err);
+
+      return callback(null);
+    }
+  );
+};
+
+const getRootAggregatorProof = (
+  segmentTree: SegmentTree<bigint, unknown, VoteProof>,
+  callback: (
+    err: Error | string | null,
+    aggregateProof?: AggregateProof
+  ) => void
+) => {
+  if (segmentTree.root instanceof InnerNode) {
+    const rootAggregatorProof = segmentTree.getCachedAggregatorProof(
+      SegmentTree.includedVotesHash(segmentTree.root.includedVotes) as bigint
+    ) as AggregateProof;
+
+    return callback(null, rootAggregatorProof);
+  }
+
+  return callback('root_error');
+};
+
+export default async (
+  data: {
+    electionId: PublicKey,
+    voteProofs: JsonProof[],
+    votersRoot: Field
+  },
+  callback: (err: Error | string | null, aggregateProof?: JsonProof) => void
+) => {
+  await Vote.compile();
+  await RangeAggregationProgram.compile();
+
+  buildTheSegmentTreeOfVoteProofs(data.voteProofs, (err, segmentTree) => {
+    if (err)
+      return callback(err);
+
+    if (!segmentTree)
+      return callback('segment_tree_error');
+
+    loadCachedAggregatorProofs(segmentTree, err => {
+      if (err)
+        return callback(err);
+
+      processSegmentTreeForAggregation({
+        segmentTree,
+        votersRoot: data.votersRoot,
+        electionId: data.electionId
+      }, err => {
+        if (err)
+          return callback(err);
+
+        getRootAggregatorProof(segmentTree, (err, rootAggregatorProof) => {
+          if (err)
+            return callback(err);
+
+          if (!rootAggregatorProof)
+            return callback('root_error');
+
+          return callback(null, rootAggregatorProof.toJSON());
+        });
+      });
+    });
+  });
 };
--- contracts/src/index.ts
+++ contracts/src/index.ts
@@ -1,6 +1,21 @@
 import { ElectionContract, setElectionContractConstants } from './ElectionContract.js';
-import { Vote, VoteProof, VotePrivateInputs, VotePublicInputs } from './VoteProgram.js';
-import { MerkleWitnessClass } from './utils.js';
 import { AggregateProof, RangeAggregationProgram } from './RangeAggregationProgram.js';
+import { InnerNode, LeafNode, SegmentTree } from './SegmentTree.js';
+import { Vote, VoteProof, VotePrivateInputs, VotePublicInputs } from './VoteProgram.js';
 
-export { AggregateProof, Vote, VoteProof, VotePrivateInputs, VotePublicInputs, MerkleWitnessClass, RangeAggregationProgram, ElectionContract, setElectionContractConstants };
+import { MerkleWitnessClass } from './utils.js';
+
+export {
+  AggregateProof,
+  ElectionContract,
+  InnerNode,
+  LeafNode,
+  MerkleWitnessClass,
+  RangeAggregationProgram,
+  SegmentTree,
+  setElectionContractConstants,
+  Vote,
+  VoteProof,
+  VotePrivateInputs,
+  VotePublicInputs
+};
